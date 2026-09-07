0c4354e112093e5a63d961cc61abac3d656f609d
Silvertide7 <natephillips801@gmail.com>
2026-09-07 09:23:43 -0600

    initial mod setup

---
+++ src/main/java/net/silvertide/quality_api/QualityAPI.java
@@ -0,0 +1,8 @@
+package net.silvertide.quality_api;
+
+import net.neoforged.fml.common.Mod;
+
+@Mod(QualityAPI.MOD_ID)
+public class QualityAPI {
+    public static final String MOD_ID = "quality_api";
+}
